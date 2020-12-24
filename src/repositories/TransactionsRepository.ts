99e002ed32330c5f8a8bda8617f44044344612c2
Elias Oliveira <eliasmatheus@hotmail.com>
2020-12-24 15:20:13 -0300

    Desafio finalizado

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTrasactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,35 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    /* eslint-disable no-param-reassign */
+    /* eslint-disable no-return-assign  */
+    const income = this.transactions.reduce((sum, transaction) => {
+      if (transaction.type === 'income') return (sum += transaction.value);
+      return sum;
+    }, 0);
+
+    const outcome = this.transactions.reduce((sum, transaction) => {
+      if (transaction.type === 'outcome') return (sum += transaction.value);
+      return sum;
+    }, 0);
+    /* eslint-enable no-return-assign  */
+    /* eslint-enable no-param-reassign */
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTrasactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,22 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const balance = this.transactionsRepository.getBalance();
+
+    const newTotal = balance.total - value;
+
+    if (newTotal <= 0 && type === 'outcome') {
+      throw Error('Balance is invalid!');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
